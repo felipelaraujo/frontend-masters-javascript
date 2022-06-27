cbc8668bd5002126a4283561ffb13683b09086d0
Felipe Araújo <fplopesaraujo@gmail.com>
2022-06-27 10:09:21 -0300

    New exercise

--- JavaScript From Fundamentals to Functional JS/002 - List Transformations/002-looping-exercise.js
+++ JavaScript From Fundamentals to Functional JS/002 - List Transformations/002-looping-exercise.js
@@ -18,3 +18,18 @@ const game = {
 for (let i = 0; i < game.suspects.length; i++) {
   console.log("suspect: ", game.suspects[i]);
 }
+
+for (let i = 0; i < game.suspects.length; i++) {
+  for (const key in game.suspects[i]) {
+    if (game.suspects[i]["name"] === "Rusty") {
+      game.suspects[i].marked = true;
+    } else {
+      game.suspects[i].marked = false;
+    }
+    console.log(
+      `index: ${i}, valor: ${game.suspects[i][key]}, ${game.suspects[i]}`
+    );
+  }
+}
+
+console.log(game);
